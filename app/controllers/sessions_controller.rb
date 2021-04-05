07cd70615ef295d868e1577be6a8f2b6009e0f11
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-05 21:03:17 +0100

    add css file for the linter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def index
-    render json: { 
-        session: session 
+    render json: {
+      session: session
     }
   end
 
   def create
-    #ser = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
+    # ser = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
     user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
 
     if user
