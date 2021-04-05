74d9c0b18e0d47aec797756ba27cc97d945528bc
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-05 18:16:26 +0100

    add sessions, users and static controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def create
+    #ser = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
+    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        logged_in: true,
+        user: user,
+        session: session
+      }
+    else
+      render json: { status: 401 }
+    end
+  end
+
+  def destroy
+    reset_session
+    render json: { message: "session destroyed" }
+  end
+end
