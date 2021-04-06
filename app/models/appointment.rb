cfcde6ee60fcc183013871e12fc019c0ee456495
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-06 19:54:08 +0100

    add validations to car and appointment models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates_presence_of :date
+  validates_presence_of :city
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,8 @@ class Car < ApplicationRecord
   has_many :pictures, as: :imageable
   has_many :appointments
   has_many :users, through: :appointments
+
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :price
 end
