b13f7bc5b90176747c4b520e52a54e035689fa5e
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-06 18:51:38 +0100

    add password validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :name
+  validates_presence_of :password
   validates_presence_of :email
   validates_uniqueness_of :email
 end
