254ea5cc0426fdce5839d9e332345df12ba420eb
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-09 08:52:11 +0100

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,30 @@
 Rails.application.routes.draw do
-  root 'static#home'
+  # root 'static#home'
+  root 'cars#index'
   resources :static, only: [:home]
   resources :sessions, only: [:create]
   delete :logout, to: 'sessions#logout'
   get :logged_in, to: 'sessions#logged_in'
+
+  namespace :admin do
+    ressources :cars do
+      ressources :pictures
+    end
+
+    resources :users do
+      resources :appointments
+    end
+
+    root to: 'cars#index'
+  end
   
   namespace :api do
     namespace :v1 do
-      resources :cars do
-        resources :pictures
+      resources :cars, only: [:index, :show] do
+        resources :pictures, Only: [:index]
       end
-      resources :users, only: [:index, :create, :show] do
-        resources :appointments
+      resources :users, only: [:create] do
+        resources :appointments, only: [:index, :create, :update, :destroy]
       end
     end
   end
