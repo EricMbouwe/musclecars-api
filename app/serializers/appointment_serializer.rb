c5ce98fe42179fb1d43c8833cdcec1fd8c31d422
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-13 16:45:45 +0100

    update car data serializer

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,3 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :city, :appointment_date, :car_id, :user_id
+  belongs_to :car
+  belongs_to :user
 end
--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,5 +1,5 @@
 class CarSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description
+  attributes :id, :name, :price, :description, :pictures
   has_many :appointments
   has_many :pictures, as: :imageable
 end
