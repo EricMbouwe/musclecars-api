0df10e26815cd40d27a50d3d3d623db8e2b542c2
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-13 13:16:50 +0100

    add delete dependance between cars and pictures

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
-  has_many :pictures, as: :imageable
+  has_many :pictures, as: :imageable, dependent: :delete_all
   has_many :appointments
   has_many :users, through: :appointments
 
