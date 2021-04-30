f37279370d2c1d6c9bb2b3d2b6a7f83a4fec0337
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-30 15:41:18 +0100

    remove manual middleware declarations from application.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Here we define what the cookies going to be structure like.
 ## the key here is the name of the session cookie
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_session_id", domain: 'musclecarsapi.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: 'musclecarsapi.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: "_session_id"
+  Rails.application.config.session_store :cookie_store, key: "_musclecars_app"
 end
 
