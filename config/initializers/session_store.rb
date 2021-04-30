42e0f59de100a275c3e66af2609175c5177791fd
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-30 15:24:10 +0100

    update session store key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,8 @@
 # Here we define what the cookies going to be structure like.
 ## the key here is the name of the session cookie
-# if Rails.env == "production"
-#  Rails.application.config.session_store :cookie_store, key: "_musclecars", :domain => :all
-# else
-#  Rails.application.config.session_store :cookie_store, key: "_musclecars"
-# end
-
-Rails.application.config.session_store :cookie_store, key: "_musclecars"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_session_id", domain: 'musclecarsapi.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: "_session_id"
+end
 
