89eaeacf26846ecf0a7f26a4b651a15a32d9c28f
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-01 00:19:25 +0100

    add skip before action verify authenticity token to application controller

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,10 @@
 ## the key here is the name of the session cookie
 ## The domain is the client production domain name
 # Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: :all || lvh.me, tld_length: 2
-# Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: "musclecarsapp.herokuapp.com"
+# Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: "musclecarsapi.herokuapp.com"
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: ".musclecarsapp.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: "musclecarsapi.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_musclecars_app"
 end
