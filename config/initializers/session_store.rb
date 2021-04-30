45ec5e43ff7860c4c35238421f9dc0a37af76ace
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-30 15:53:31 +0100

    update session store client production domain name to all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,10 @@
 # Here we define what the cookies going to be structure like.
 ## the key here is the name of the session cookie
 ## The domain is the client production domain name
+#Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: 'musclecarsapp.herokuapp.com'
+
 if Rails.env == "production"
-  # Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: :all
-  Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: 'musclecarsapp.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: "_musclecars_app", domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: "_musclecars_app"
 end
