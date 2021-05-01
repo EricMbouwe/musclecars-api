7bb45bab9b42f45755884252f7645f9f3750c672
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-01 01:27:08 +0100

    add skip before action verify authenticity token to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+  skip_before_action :protect_from_forgery
   before_action :set_current_user
   before_action :require_login
 
